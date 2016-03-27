f4d60a5bc0c7e0a36947cf75433c245b6df6d6d2
Monika Hrda <monika.h79@gmail.com>
2016-03-27 23:50:56 +0100

    Got rid of RequestAware and ModelDriven interfaces in ShowProfileAction.

--- src/main/java/mhrda/socialme/actions/ShowProfileAction.java
+++ src/main/java/mhrda/socialme/actions/ShowProfileAction.java
@@ -1,29 +1,25 @@
 package mhrda.socialme.actions;
 
-import java.util.Map;
-
-import org.apache.struts2.interceptor.RequestAware;
-
+import org.apache.struts2.ServletActionContext;
 import mhrda.socialme.entities.User;
 import mhrda.socialme.interceptors.UserAware;
 
-import com.opensymphony.xwork2.ModelDriven;
-
-public class ShowProfileAction extends BaseAction implements ModelDriven<User>, RequestAware, UserAware {
+public class ShowProfileAction extends BaseAction implements UserAware {
 
 	private static final long serialVersionUID = 1L;
-	
-	private User profileUser = new User();
+		
+	private User profileUser;
 	private User loggedInUser;
-	private Map<String,Object> requestAttributes;
+	private String profileId;
+	
 
 	@Override
 	public String execute() throws Exception {
-		int userId = (int) requestAttributes.get("userId");
-		if (userId == 0) {
-			userId = loggedInUser.getUserId();
+		String profileId = ServletActionContext.getRequest().getParameter("userId");
+		if (profileId == null) {
+			profileId = Integer.toString(loggedInUser.getUserId());
 		}
-		profileUser = getUserDAO().getUserById(userId);
+		profileUser = getUserDAO().getUserById(Integer.parseInt(profileId));
 		return SUCCESS;
 	}
 	
@@ -39,15 +35,9 @@ public class ShowProfileAction extends BaseAction implements ModelDriven<User>, 
 	public void setLoggedInUser(User loggedInUser) {
 		this.loggedInUser = loggedInUser;
 	}
-	
-	@Override
-	public User getModel() {
-		return this.profileUser;
-	}
-	
-	@Override
-	public void setRequest(Map<String, Object> request) {
-		this.requestAttributes = request;
-	}
 
+	public String getProfileId() {
+		return profileId;
+	}
+	
 }
